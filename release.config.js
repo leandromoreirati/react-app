8355461af59c3ffc68bc55bbb3cea3624ec07437
Leandro Moreira <leandro@leandromoreirati.com.br>
2022-06-26 13:57:14 -0300

    fix: upload.

--- release.config.js
+++ release.config.js
@@ -5,6 +5,12 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
-    "@semantic-release/github",
+   [ "@semantic-release/github", {
+        assets: [
+          { path: "build.zip", label: "build"},
+          { path: "coverage.zip", label: "Coverage"}
+        ]
+
+   }]
   ],
 };
